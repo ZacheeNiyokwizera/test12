33c125b3d41ebfb4aeac417ee8d565e084cb5904
Zachée Niyokwizera <53909831+zackniyokwizera@users.noreply.github.com>
2022-09-28 20:42:29 +0200

    Add files via upload
    Test commit

---
+++ HTML/app.js
@@ -0,0 +1,81 @@
+// console.log("Starting")
+
+// let car = {
+//    modal: 'Ford',
+//    color:'black',
+//    year: 2000,
+// }
+
+// console.log(car.year)
+
+
+// let btn = document.getElementById('btn');
+
+
+// let paragraph2 = document.createElement('p');
+
+// paragraph2.innerHTML = "My name is Zachee, and i'm a programmer.";
+// paragraph2.style.fontSize = "18px";
+// paragraph2.style.fontFamily = "sans-serif";
+// paragraph2.style.width = "100px";
+// paragraph2.style.border = "1px solid orange";
+// paragraph2.style.marginLeft = "36px";
+// paragraph2.style.padding = "30px"; 
+
+
+
+// paragraph2.addEventListener('mouseover', changePar);
+
+
+// function changePar(){
+
+//     paragraph2.style.color = "yellow";
+
+//     // document.body.style.backgroundColor = 'red';
+
+
+// }
+
+// document.body.appendChild(paragraph2);
+
+
+// 1. Create elements
+// 2. Access elements
+// 3. Manipulate
+// 4. Deleting elements 
+
+// BY ID
+// BY CLASSNAME
+//BY TAG NAME
+
+let myBtns = document.getElementsByClassName('.button');
+let myBttns = document.querySelectorAll('.button');
+
+
+console.log(myBttns);
+
+
+// let myBtn = document.getElementsByTagName('button');
+
+
+// console.log(myBtn);
+
+// let x = myBtn[2]; 
+
+// x.innerHTML = "Testing";
+// console.log(x);
+
+
+
+// let btn = document.getElementById('btn');
+
+// btn.addEventListener('click', changeText);
+
+// function changeText(){
+ 
+//  btn.innerHTML = "Connecting...";
+//  document.body.style.backgroundColor = 'red';
+
+// }
+
+
